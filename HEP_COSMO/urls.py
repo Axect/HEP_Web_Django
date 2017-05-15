265f65ab4f200081948177798b9e7607e4880043
Axect <edeftg@gmail.com>
2017-05-16 01:58:30 +0900

    JS update (Home, Members)

--- HEP_COSMO/urls.py
+++ HEP_COSMO/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^index', views.index, name='index'),
     url(r'^members', views.members, name='members'),
 ]
