c2eca48825f489b05cf0edbf02a417a6387d4df5
Axect <edeftg@gmail.com>
2017-05-17 10:06:48 +0900

    Upgrade Model

--- HEP_COSMO/admin.py
+++ HEP_COSMO/admin.py
@@ -14,3 +14,14 @@ class ArticleAdmin(admin.ModelAdmin):
     list_per_page = 25
 
 admin.site.register(Article, ArticleAdmin)
+
+
+class PeopleAdmin(admin.ModelAdmin):
+    '''
+        Admin View for People
+    '''
+    list_display = ('position', 'index', 'name', 'email', 'location', 'link')
+    list_display_links = ('name',)
+    search_fields = ('position', 'name')
+
+admin.site.register(People, PeopleAdmin)
--- HEP_COSMO/models.py
+++ HEP_COSMO/models.py
@@ -10,8 +10,10 @@ class Article(models.Model):
     index = models.CharField(max_length=5)
     people = models.CharField(max_length=200)
     title = models.CharField(max_length=200)
-    journal = models.CharField(max_length=200)
-    hyperlink = models.TextField()
+    arxiv = models.CharField(max_length=200, null=True, blank=True)
+    arxivlink = models.TextField(null=True, blank=True)
+    journal = models.CharField(max_length=200, null=True, blank=True)
+    hyperlink = models.TextField(null=True, blank=True)
     created_date = models.DateTimeField(
         default=timezone.now
     )
@@ -25,3 +27,29 @@ class Article(models.Model):
 
     def __str__(self):
         return self.title
+
+class People(models.Model):
+    """Model definition for People."""
+
+    # TODO: Define fields here
+    position = models.CharField(max_length=30)
+    index = models.CharField(max_length=5)
+    name = models.CharField(max_length=30)
+    email = models.CharField(max_length=100)
+    location = models.CharField(max_length=20)
+    link = models.TextField(null=True, blank=True)
+    img = models.TextField(null=True, blank=True)
+    
+
+    class Meta:
+        """Meta definition for People."""
+
+        verbose_name = 'People'
+        verbose_name_plural = 'Peoples'
+
+    def __unicode__(self):
+        """Unicode representation of People."""
+        pass
+
+    def __str__(self):
+        return self.name
