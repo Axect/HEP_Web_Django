58e0ca1e199cda394946bc9e42ac2dd885db3e0f
Axect <edeftg@gmail.com>
2017-05-17 11:44:33 +0900

    Modify DB

--- HEP_COSMO/admin.py
+++ HEP_COSMO/admin.py
@@ -8,7 +8,7 @@ class ArticleAdmin(admin.ModelAdmin):
     '''
         Admin View for Article
     '''
-    list_display = ('year','index',  'people', 'title', 'journal', 'author', 'published_date')
+    list_display = ('year','index',  'people', 'title', 'arxiv', 'journal', 'published_date')
     list_display_links = ('people', 'title',)
     search_fields = ('year', 'title', 'people')
     list_per_page = 25
